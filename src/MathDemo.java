e8c54ab229a15751b6f209a222e2f42bb91c76d6
PAAG <keapaagdat23a@gmail.com>
2023-08-28 10:06:21 +0200

    MathDemo.java tilføjet

---
+++ src/MathDemo.java
@@ -0,0 +1,20 @@
+public class MathDemo {
+  public static void main(String[] args) {
+    /*
+    Prøv dig frem med Math-metoderne.
+    Hvad gør de nedenstående Math-metoder?
+     */
+
+    Math.abs();
+    Math.max();
+    Math.min();
+    Math.floor();
+    Math.ceil();
+    Math.exp();
+    Math.pow();
+    Math.round();
+    Math.sqrt();
+    Math.random();
+    Math.PI;
+  }
+}
