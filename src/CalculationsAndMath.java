4e1f758f5220b4de39faa7c405534965a3287cba
PAAG <keapaagdat23a@gmail.com>
2023-08-28 09:57:54 +0200

    Beregninger tilføjet

--- src/CalculationsAndMath.java
+++ src/CalculationsAndMath.java
@@ -1,4 +1,19 @@
 public class CalculationsAndMath {
+  public static void main(String[] args) {
+    int num = 13;
+    System.out.println("Result: " + num / 3);
+    System.out.println("Remain: " + num % 3);
+
+    System.out.println("Even or odd: " + (num % 2));
+
+    int num1 = 5;
+    int num2 = 3;
+    int num3 = 4;
+
+    System.out.println(num3 * (num1 + num2));
+
+    System.out.println(2 * -3);
+  }
 }
 
-// https://forms.office.com/Pages/ResponsePage.aspx?id=bjwM0SjCREmLamBnxq_jyVReFEbgIBtPlWe1UDlxQdpUMFlTQzBTTDFIRUswSDM2UlRKU0REWEw5RS4u
+// https://forms.ffice.com/Pages/ResponsePage.aspx?id=bjwM0SjCREmLamBnxq_jyVReFEbgIBtPlWe1UDlxQdpUMFlTQzBTTDFIRUswSDM2UlRKU0REWEw5RS4u
